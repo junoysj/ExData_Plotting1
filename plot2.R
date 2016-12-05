c4b6d6a9e660f6dd304e8c44a91394bb8976a8f6
Shujing(Juno) Yu <junoyu1231@gmail.com>
2016-12-05 14:35:11 -0500

    Add files via upload

--- plot2.R
+++ plot2.R
@@ -8,6 +8,9 @@ subset = subset(data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
 datetime <- paste(as.Date(subset$Date), subset$Time)
 subset$Datetime <- as.POSIXct(datetime)
 
+plot(subset$Global_active_power~subset$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+
 #save to a PNG file with a width of 480 pixels and a height of 480 pixels
 dev.copy(png, file="plot2.png", height=480, width=480)
 dev.off()
